e292df54f26dc1e9bc5621a5d9c248c074ae60f0
ivysandberg <ins5kx@virginia.edu>
2018-08-23 14:06:15 -0400

    update trade-terror

--- code/trade-terrorism/trade-terror-classification.py
+++ code/trade-terrorism/trade-terror-classification.py
@@ -29,9 +29,10 @@ df = pd.read_csv(
     '/Users/ivysandberg/mycode/PEconPy/data/trade-terrorism_classification.csv'
 )
 df = pd.DataFrame(df)
+print("Original Data:")
 print(df.head())
+''' DEFINE NEW TARGET VARIABLE: LEVEL OF TERRORISM '''
 
-# create new target column with ranges for terrorist activity
 terrorism = []
 
 for i in df['trans_terrorism_cj']:
@@ -46,28 +47,42 @@ for i in df['trans_terrorism_cj']:
         terrorism.append(i)
 
 df['terrorism'] = terrorism
+print("New Terrorism Target variable:")
 print(df['terrorism'])
 
+print("Data including the new target variable:")
 print(df.head())
 print(df.info())
 
+print("Total counts for each category of terrorism:")
 print(df.groupby('terrorism').count())
 
 # examine distribution of target variable (in continuous form)
 plt.figure()
 sns.countplot(x='trans_terrorism_cj', data=df)
 plt.xticks(range(0, 15))
-plt.show()
+plt.xlabel("Number of transnational terrorist events")
+#plt.show()
 
 # Create arrays for the features and target variable
 y = df['terrorism'].values
-X = df.drop(['terrorism', 'country2', 'country1'], axis=1).values
+X = df.drop(
+    ['terrorism', 'trans_terrorism_cj', 'country1', 'country2'], axis=1).values
+X_df = pd.DataFrame(X)
+print("This is the training data:")
+print(X_df.head())
+X_df.columns = [
+    'year', 'border', 'comlang', 'Currency_union', 'island', ',landl',
+    'log_dist', 'log_RGDP', 'log_RGDP_percap', 'regional_trade_agreement',
+    'total_exports', 'primary_exports', 'manfactured_exports', 'total_imports',
+    'primary_imports', 'manfactured_imports', 'exp_index', 'imp_index'
+]
 
 # split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=21, stratify=y)
+''' K Nearest Neighbors '''
 
-# K Nearest Neighbors
 # Create a for loop to test different values of k
 neighbors = np.arange(1, 9)
 # make an array to story test accuracies
@@ -83,17 +98,19 @@ plt.title('KNN Vary Value of K (n_neighbors)')
 plt.plot(neighbors, test_accuracy, label='Testing Accuracy')
 plt.xlabel('Number of Neighbors')
 plt.ylabel('Accuracy')
-plt.show()
+#plt.show()
 # neighbors=7 showed the highest accuracy
 
 y_pred = model.predict(X_test)
+print("KNN accuracy score:")
 print(model.score(X_test, y_test))
 
 # Evaluate level of confidence in the model
 print(confusion_matrix(y_test, y_pred))
+print("KNN Classification report:")
 print(classification_report(y_test, y_pred))
+''' Compare classifiers '''
 
-# Compare classifiers
 # list all the names of the Classifiers
 names = [
     'K Nearest Neighbors', 'Gaussian Naive Bayes', 'Multinomial Naive Bayes',
@@ -123,36 +140,39 @@ for name, model in zip(names, classifiers):
 
 results = dict(zip(names, score))
 
+print(
+    "Classification algorithms accuracy scores on prediciting terrorism levels:"
+)
 pprint(results)
 
 # Random Forest Classifier (bootstrap aggreagted decision trees)
 RFmodel = RandomForestClassifier(max_depth=4, random_state=0)
 RFmodel.fit(X_train, y_train)
 
+cv_scores = cross_val_score(RFmodel, X, y, cv=5)
+print("RandomForestClassifier Cross Validation Scores:")
+print(cv_scores)
+
+# Explore the best performing model
+ABmodel = AdaBoostClassifier()
+ABmodel.fit(X_train, y_train)
+
 # print feature importances, the higher the number the more important
-#print(RFmodel.feature_importances_)
 
 feats = {}  # a dict to hold feature_name: feature_importance
-for feature, importance in zip(df.columns, RFmodel.feature_importances_):
+for feature, importance in zip(X_df.columns, ABmodel.feature_importances_):
     feats[feature] = importance  #add the name/value pair
 
+print("AdaBoost Feature Importances:")
 pprint(feats)
 
 plt.hist(y_test)
-plt.show()
+#plt.show()
 
-preds = RFmodel.predict(X_test)
+plt.scatter(df['primary_exports'], df['trans_terrorism_cj'])
+#plt.show()
 
-cv_scores = cross_val_score(RFmodel, X, y, cv=5)
-print(cv_scores)
-
-X_test_df = pd.DataFrame(X_test)
-X_test_df['preds'] = preds
-
-print(X_test_df.head())
-print(X_test_df.groupby("preds").count())
-
-print(X_test_df.loc[X_test_df['preds'] == "Many events"])
-
-plt.scatter(df['exp_index'], df['trans_terrorism_cj'])
-plt.show()
+new_data = pd.read_csv(
+    '/Users/ivysandberg/Desktop/USxAustria_deltaExportValue.csv')
+new_pred = ABmodel.predict(new_data)
+#print(new_pred)
